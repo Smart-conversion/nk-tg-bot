63bb09064f2f119c93a5a0a0831ad63b0e8a0670
Алина Борисова <b-alina1999.borisova@yandex.ru>
2024-12-20 00:09:08 +0300

    edit

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const TelegramApi = require('node-telegram-bot-api');
+const token='7648296652:AAGxzvepiVi7Zy24rxnA12tJNhZm6TUnUJs'
 
-const bot = new TelegramApi(process.env.BOT_API_KEY, { polling: true });
+//const bot = new TelegramApi(process.env.BOT_API_KEY, { polling: true });
+const bot = new TelegramApi(token, { polling: true });
 const channelId = '-1001792092494';
 
 const buttonNext_1 = {
